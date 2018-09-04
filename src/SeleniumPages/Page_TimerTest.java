6dd6b6d156f93edc0283bd335a370d41d895ff68
himadri <sinhhimadri@gmail.com>
2018-09-04 22:42:33 +0100

    Updated the feature files with new scenario

--- src/SeleniumPages/Page_TimerTest.java
+++ src/SeleniumPages/Page_TimerTest.java
@@ -1,31 +1,33 @@
 package SeleniumPages;
 
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import common.Page_BasePage;
-import junit.framework.Assert;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
 
 public class Page_TimerTest extends Page_BasePage{
 
 	public void launchBrowser() {
 		System.setProperty("webdriver.chrome.driver", "C:\\softwares\\chromedriver_win32\\chromedriver.exe");
-		//driver = new ChromeDriver();
-		//ChromeOptions options = new ChromeOptions();
-		//options.addArguments("disable-infobars");		
-		//WebDriver driver = new ChromeDriver(options);
-		
+			
 	}
 	
 	public void openTimerURL() {
-		//driver.get("http://www.google.com");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("disable-infobars");		
 	    driver = new ChromeDriver(options);
@@ -75,7 +77,6 @@ public class Page_TimerTest extends Page_BasePage{
 	}	
 	
 	
-	
 	public void validatetimertun(int time) {		
 		
 		//driver.manage().timeouts().implicitlyWait(time, TimeUnit.SECONDS);
@@ -87,7 +88,7 @@ public class Page_TimerTest extends Page_BasePage{
 		
 		
 		try {
-		    WebDriverWait wait = new WebDriverWait(driver, time);
+		   WebDriverWait wait = new WebDriverWait(driver, time);
 		    wait.until(ExpectedConditions.alertIsPresent());
 		    Alert alert = driver.switchTo().alert();
 		    System.out.println(alert.getText());
@@ -98,7 +99,87 @@ public class Page_TimerTest extends Page_BasePage{
 		}
 		
 	}	
-	
+
+
+public void runTimer()
+ {
+	 long startTime=0;
+
+     try {       
+
+            String prevText = new String();
+            String currText = new String();
+            FluentWait<WebDriver> Wait = new FluentWait<WebDriver>(driver);
+            Wait.withTimeout(25, TimeUnit.SECONDS);
+            Wait.pollingEvery(1, TimeUnit.MILLISECONDS);
+            Wait.ignoring(NoSuchElementException.class);
+            startTime = System.currentTimeMillis();       
+
+            Wait.until(new Function<WebDriver, Boolean>() {
+
+            				public Boolean apply(WebDriver driver) {
+
+            				System.out.println("Current Window State       : "
+
+                			+ String.valueOf(((JavascriptExecutor) driver).executeScript("return document.readyState")));
+
+            				return String
+
+            						.valueOf(((JavascriptExecutor) driver).executeScript("return document.readyState"))
+
+            						.equals("complete");
+
+            				} });
+
+            //Wait.until(ExpectedConditions.textToBePresentInElementLocated(By.id("progressText"), "25 seconds")); 
+            prevText = driver.findElement(By.id("progressText")).getText();
+            long timeLapsed = System.currentTimeMillis();
+            while(driver.findElement(By.id("progressText")).isDisplayed())
+            {   
+            	long loopTimeStart =  System.currentTimeMillis();
+            	currText = driver.findElement(By.id("progressText")).getText();
+            	//System.out.println(currText);
+               	if (currText.equals(prevText))
+                                {
+
+                         			continue;
+
+                                }
+
+                   else
+
+                   {
+
+                       long changeTime = System.currentTimeMillis();
+                         System.out.println(currText);
+                         System.out.println("Time Lapsed : " + (changeTime - timeLapsed) );              
+                                              
+                        //Assert.assertTrue("Test failed", Math.abs(1000 - (changeTime - timeLapsed)) < 50);
+                         
+                         timeLapsed = changeTime;        
+                         prevText = currText;                
+                         long loopTimeEnds =  System.currentTimeMillis();
+                         System.out.println("Loop time : " + (loopTimeStart - loopTimeEnds ) );    
+                   }
+            }
+            long endTime = System.currentTimeMillis();
+            System.out.println("Total Time Lapsed : " + (startTime - endTime ) );       
+     }
+
+      catch (Exception e) {
+
+         //exception handling     
+            long endTime = System.currentTimeMillis();
+            System.out.println("Total Time Lapsed : " + (startTime - endTime ) );
+            
+            if ((startTime - endTime ) < 2500)
+            {
+            	System.out.println("Test Failes as Total tme lapsed is less than 2500");
+            }
+      }
+     closethebrowser();
+   	 
+  }
    public void closethebrowser() {		
 		
 		driver.close();
